42c42c2bab340eee09c88edf5d5a0aeb5acb8920
syn22 <syn22.eth@gmail.com>
2023-06-07 17:14:46 +0700

    update port handling

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const placesRoutes = require('./routes/places');
 const usersRoutes = require('./routes/users');
 const plansRoutes = require('./routes/plans');
+const getPort = require('get-port');
 
 const app = express();
-const port = process.env.PORT || 5001;
 let server;
 
 app.use(cors());
@@ -14,18 +14,27 @@ app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/plans', plansRoutes);
 
-// Start the server
-server = app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Find an available port dynamically
+getPort().then((port) => {
+  // Start the server
+  server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch((error) => {
+  console.error('Failed to find an available port:', error);
+  process.exit(1);
 });
 
 // Gracefully handle termination signal
-process.on('SIGTERM', () => {
-  console.log('Received SIGTERM. Closing the server...');
+const handleTermination = () => {
+  console.log('Closing the server...');
   server.close(() => {
     console.log('Server closed. Exiting process...');
     process.exit(0);
   });
-});
+};
+
+process.on('SIGTERM', handleTermination);
+process.on('SIGINT', handleTermination);
 
 module.exports = app;
